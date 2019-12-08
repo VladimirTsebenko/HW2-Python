3478f8e149a93cbc332d5ccbd882d8cf686568b1
Vladimir <v.tsebenko@gmail.com>
2019-12-08 19:50:06 +0200

    fib

--- hw2.py
+++ hw2.py
@@ -1,5 +1,26 @@
 # 1.Рассчитать последовательность фибоначчи, длину ряда задает пользователь
 
+number = int(input("Enter Fibonacci length: "))
+print ("Fibonacci number of: ", number, "is: ", round(((5**(0.5)+1)/2)**number/(5**(0.5))))
+
+p = range(number+1)
+
+fibonacci = []
+
+for i in p:
+    
+    if(i == 0):
+        n = 0
+        fibonacci.append(n)
+    elif(i == 1):
+        n = 1
+        fibonacci.append(n)
+    elif(i > 1):
+        n = fibonacci[i-1] + fibonacci[i-2]
+        fibonacci.append(n)
+
+print("Fibonacci sequence is: ", fibonacci)
+
 # 2. Дан текстовый файл, содержащий целые числа. Удалить из него все четные числа.
 
 # 3. Дан текстовый файл. Создать новый файл, каждая строка которого получается из соответствующей строки
